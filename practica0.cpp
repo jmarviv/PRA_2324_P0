adb9e96e04afc0cbc9723ad9b961f4311d039773
Jorge Marcos Vives <jorgemarcosvives@gmail.com>
2023-09-20 19:49:28 +0200

    he hecho el main

---
+++ practica0.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "brazorobotico.h"
+using namespace std;
+
+
+int main(){
+	BrazoRobotico robot = BrazoRobotico();
+	robot.mover(90,90,90);
+	robot.sujetar();
+	robot.cojer();
+	cout<<robot.consulx()<<endl;
+	cout<<robot.consuly()<<endl;
+	cout<<robot.consulz()<<endl;
+	cout<<robot.consulsujetar()<<endl;
+	return 0;
+}
